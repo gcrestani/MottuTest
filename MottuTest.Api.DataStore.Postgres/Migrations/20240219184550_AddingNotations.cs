85bf8836ba96c687a36dcd94d8e321eefe03e9b7
gcrestani <gcrestanis@gmail.com>
2024-02-19 16:01:48 -0300

    Adding data notations to entity framework

---
+++ MottuTest.Api.DataStore.Postgres/Migrations/20240219184550_AddingNotations.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MottuTest.Api.DataStore.Postgres.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingNotations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ShortUrl",
+                table: "Urls",
+                type: "character varying(120)",
+                maxLength: 120,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OriginalUrl",
+                table: "Urls",
+                type: "character varying(120)",
+                maxLength: 120,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ShortUrl",
+                table: "Urls",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(120)",
+                oldMaxLength: 120);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OriginalUrl",
+                table: "Urls",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(120)",
+                oldMaxLength: 120);
+        }
+    }
+}
--- MottuTest.Model/Entities/Url.cs
+++ MottuTest.Model/Entities/Url.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,14 @@ namespace MottuTest.Model.Models
 {
   public class UrlDto : BaseEntity
   {
+    [Required]
+    [DefaultValue(0)]
     public int Hits { get; set; }
+    [Required]
+    [MaxLength(120)]
     public string OriginalUrl { get; set; }
+    [Required]
+    [MaxLength(120)]
     public string ShortUrl { get; set; }
   }
 }
